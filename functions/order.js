b233cd0f63f5caa129a6bf471fedf91ab73a5abb
Admin <iheachochristian@yahoo.com>
2024-11-23 18:41:56 +0000

    Nexus app setup

--- functions/order.js
+++ functions/order.js
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 
 const supabase_url = process.env.SUPABASE_URL;
 const supabase_key = process.env.SUPABASE_KEY;
