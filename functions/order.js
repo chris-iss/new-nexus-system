1e95d45db2cd644ada0bb06c99e5e29dd76ae603
Admin <iheachochristian@yahoo.com>
2024-11-23 17:48:06 +0000

    Nexus app setup

--- functions/order.js
+++ functions/order.js
@@ -21,7 +21,7 @@ export const handler = async (event) => {
     isExecuting = true;
 
     try {
-        // Validate request body
+        // Validate request bodys
         if (!event.body) {
             console.error("Empty body received");
             isExecuting = false;
