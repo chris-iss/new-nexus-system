7c4d8d6222ba214d67c81865474d346b1c54496e
Admin <iheachochristian@yahoo.com>
2024-11-23 20:47:37 +0000

    Nexus app setup

--- functions/order.js
+++ functions/order.js
@@ -33,28 +33,39 @@ export const handler = async (event) => {
         console.log("REQUESST BODY:", requestBody)
 
         // Extract data from request
-        const fName = requestBody.billing.first_name;
-        const lastName = requestBody.billing.last_name;
-        const course = requestBody.line_items[0]?.name;
-        const quantity = requestBody.line_items[0]?.quantity;
-        const amount = requestBody.line_items[0]?.subtotal;
-        const status = requestBody.status;
-        const currencySymbol = requestBody.currency_symbol
-        const date = new Date();
+        // const fName = requestBody.billing.first_name;
+        // const lastName = requestBody.billing.last_name;
+        // const course = requestBody.line_items[0]?.name;
+        // const quantity = requestBody.line_items[0]?.quantity;
+        // const amount = requestBody.line_items[0]?.subtotal;
+        // const status = requestBody.status;
+        // const currencySymbol = requestBody.currency_symbol
+        // const date = new Date();
 
-        const insertOrder = {
-            firstname: fName,
-            lastname: lastName,
-            course: course,
-            quantity: quantity,
-            amount: amount,
-            status: status,
-            currency: currencySymbol,
-            date: date,
-        };
+        // const insertOrder = {
+        //     firstname: fName,
+        //     lastname: lastName,
+        //     course: course,
+        //     quantity: quantity,
+        //     amount: amount,
+        //     status: status,
+        //     currency: currencySymbol,
+        //     date: date,
+        // };
 
+        const proccessOrder = requestBody.line_items.map((item) => {
+            fName = requestBody.billing.first_name;
+            lastName = requestBody.billing.last_name;
+            course = item.name;
+            quantity = item.quantity;
+            amount = item.price * item.quantity;
+            status = requestBody.status;
+            currencySymbol = requestBody.currency_symbol
+            date = new Date();
+
+        })
         // Save data to Supabase
-        const { data, error } = await supabase.from("orders").insert([insertOrder]);
+        const { data, error } = await supabase.from("orders").insert([proccessOrder ]);
 
         if (error) {
             console.error("Error inserting into Supabase:", error);
