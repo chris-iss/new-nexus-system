999a1213215a2437aaac9a8e70cbd8ac0f5b2ce7
Admin <iheachochristian@yahoo.com>
2024-11-24 20:07:40 +0000

    Nexus app setup

---
+++ functions/order_payment.js
@@ -0,0 +1,37 @@
+import { supabase } from "../utils/database";
+
+const supabase_url = process.env.SUPABASE_URL;
+const supabase_service_key = process.env.SERVICE_KEY;
+
+export const supabase = createClient(supabase_url, supabase_service_key);
+
+exports.handler = async (event) => {
+    let isExecuting = false;
+
+    if (isExecuting) {
+        return {
+            statusCode: 409,
+            body: JSON.stringify({ message: "Function is already executing" }),
+        };
+    }
+
+    isExecuting = true;
+
+    try {
+        if (!event.body) {
+            console.error("Empty body received");
+            isExecuting = false;
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: "Request body is empty or missing" }),
+            };
+        }
+
+        const requestBody = JSON.parse(event.body);
+
+        console.log("REQUEST BODY:", requestBody);
+
+    } catch(error) {
+        console.log("ERROR", error.message)
+    }
+}
