2387d05b5e8c46694034dc01875820e2017bb710
Admin <iheachochristian@yahoo.com>
2024-12-14 14:57:30 +0000

    Nexus app setup

---
+++ functions/completed_module.js
@@ -0,0 +1,77 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabase_url = process.env.SUPABASE_URL;
+const supabase_service_key = process.env.SERVICE_KEY;
+
+export const supabase = createClient(supabase_url, supabase_service_key);
+
+export const handler = async (event) => {
+    let isExecuting = false;
+
+    if (isExecuting) {
+        return {
+            statusCode: 409,
+            body: JSON.stringify({ message: "Function is already executing" }),
+        };
+    }
+
+    isExecuting = true;
+
+    try {
+        // Validate request body
+        if (!event.body) {
+            console.error("Empty body received");
+            isExecuting = false;
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: "Request body is empty or missing" }),
+            };
+        }
+
+        const requestBody = JSON.parse(event.body);
+
+        console.log("REQUEST BODY:", requestBody);
+
+        // Process each order items
+        // const assessment_data = {
+        //     assessment_id: requestBody.assessment_id,
+        //     firstname: requestBody.firstname,
+        //     lastname: requestBody.lastname,
+        //     email: requestBody.email,
+        //     score: requestBody.score,
+        //     attempt: requestBody.attempt,
+        //     status: requestBody.status,
+        //     date_started: requestBody.date_started,
+        //     date_completed: requestBody.date_completed
+        // }
+
+        // Save data to Supabase
+        // const { data, error } = await supabase.from("assessment_one").insert(assessment_data);
+
+        // if (error) {
+        //     console.error("Error inserting into Supabase:", error);
+        //     return {
+        //         statusCode: 500,
+        //         body: JSON.stringify({ message: "Error inserting into Supabase", error: error.message }),
+        //     };
+        // }
+
+        // Return success response
+        isExecuting = false;
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        };
+    } catch (error) {
+        console.error("Error processing data:", error.message);
+
+        // Reset execution flag
+        isExecuting = false;
+
+        // Return error response
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Internal Server Error", error: error.message }),
+        };
+    }
+};
