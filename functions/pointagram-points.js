8fd80957c6ba97d55a5d2b5a3e1369c5ac6a28cf
Christian Iheacho <christianiheacho@Christians-MacBook-Air.local>
2025-04-30 09:08:17 +0100

    added thinkifc profile update function

--- functions/pointagram-points.js
+++ functions/pointagram-points.js
@@ -115,11 +115,12 @@ export async function handler(event, context) {
     // Step 3: Fetch all player details (for email and team)
     const allPlayersRes = await fetch(`${API_URL}/players`, { headers });
     const allPlayersData = await allPlayersRes.json();
-    console.log("ALL PLAYERS:", allPlayersData.data.players)
     const allPlayers = allPlayersData?.data?.players || [];
 
     // Step 4: Combine leaderboard and player details
     const enrichedLeaderboard = leaderboardPlayers.map(lbPlayer => {
+      console.log("LEADERBOARD-PLAYER-IDS:", lbPlayer.id)
+      console.log("PLAYER-IDS", allPlayersData?.data?.players)
       const playerInfo = allPlayers.find(p => p.id === lbPlayer.id);
 
       return {
