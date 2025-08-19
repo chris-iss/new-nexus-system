d9638ef431faa247b3a8ed651feff9de0d412181
chris-iss <christian@instituteofsustainabilitystudies.com>
2025-08-19 09:06:14 +0100

    Implement whitepaper function

---
+++ functions/whitepaper_update.js
@@ -0,0 +1,73 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase_url = process.env.SUPABASE_URL;
+const supabase_service_key = process.env.SERVICE_KEY;
+
+export const supabase = createClient(supabase_url, supabase_service_key);
+
+export const handler = async (event) => {
+    let isExecuting = false;
+
+    if (isExecuting) {
+        return {
+            statusCode: 409,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({ message: "Function is already executing" }),
+        };
+    }
+
+    isExecuting = true;
+
+    try {
+        // Handle CORS preflight (OPTIONS request)
+        if (event.httpMethod === "OPTIONS") {
+            return {
+                statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+                    "Access-Control-Allow-Headers": "Content-Type",
+                },
+                body: "",
+            };
+        }
+
+        // Example: Fetch data from 'eventUpdate' table
+        const { data, error } = await supabase
+            .from('white_paper')
+            .select('*');
+
+        if (error) {
+            console.error("Supabase Error:", error);
+            return {
+                statusCode: 500,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                },
+                body: JSON.stringify({ error: error.message })
+            };
+        }
+
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify(data)
+        };
+    } catch (error) {
+        console.error("Error processing data:", error.message);
+
+        return {
+            statusCode: 500,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({ message: "Internal Server Error", error: error.message }),
+        };
+    } finally {
+        isExecuting = false;
+    }
+};
